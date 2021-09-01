e38e5f22e27742d3b3c35e1b87558564a41c1293
lkimilhol <lkimilhol@gmail.com>
2021-09-01 22:50:11 +0900

    Add files via upload

---
+++ src/main/java/com/example/jordyshop/JordyShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.jordyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JordyShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JordyShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jordyshop/JordyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jordyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JordyShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
