3ee445558775a4b6bcd78b7db34e18fc65f75df9
bbbnam <bbbnam@naver.com>
2021-09-15 00:54:07 +0900

    feat : 암호화 알고리즘(aes, bcrypt) enum 생성

---
+++ src/main/java/com/deliverysaurus/common/encryption/EncryptionStrategy.java
@@ -0,0 +1,45 @@
+package com.deliverysaurus.common.encryption;
+
+import com.deliverysaurus.common.encryption.aes.AES256;
+import com.deliverysaurus.common.encryption.bcryption.BCryption;
+
+public enum EncryptionStrategy {
+    AES256 {
+        @Override
+        public String encrypt(String text) throws Exception {
+            return new AES256().encrypt(text);
+        }
+
+        @Override
+        public String decrypt(String text) throws Exception {
+            return new AES256().decrypt(text);
+        }
+
+        @Override
+        public boolean isMatch(String text, String hashed) throws Exception {
+            return text.equals(new AES256().decrypt(text));
+        }
+    },
+    BYCRYPT {
+        @Override
+        public String encrypt(String text) {
+            return new BCryption().encrypt(text);
+        }
+
+        @Override
+        public String decrypt(String text) {
+            throw new RuntimeException();
+        }
+
+        @Override
+        public boolean isMatch(String text, String hashed) {
+            return new BCryption().isMatch(text, hashed);
+        }
+    };
+
+    public abstract String encrypt(String text) throws Exception;
+
+    public abstract String decrypt(String text) throws Exception;
+
+    public abstract boolean isMatch(String text, String hashed) throws Exception;
+}
