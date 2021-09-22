380ba4683d302f5e340146cae32a5d68b8d2494f
lkimilhol <lkimilhol@gmail.com>
2021-09-22 10:59:12 +0900

    refactor(MemberTest): 상수 제거

--- src/test/java/com/deliverysaurus/member/domain/MemberTest.java
+++ src/test/java/com/deliverysaurus/member/domain/MemberTest.java
@@ -6,21 +6,31 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class MemberTest {
+
+    private static final String NAME = "죠르디";
+    private static final String NICKNAME = "jordy";
+    private static final String ZIP_CODE = "05552";
+    private static final String ADDRESS = "서울시 영등포구 영등포동 포동길 12";
+    private static final String ADDRESS_DETAIL = "101호";
+    private static final String TEL = "01015251525";
+    private static final int AGE = 15;
+    private static final String PASSWORD = "password";
+
     @DisplayName("생성")
     @Test
     void create() {
         // given
         Member member = new Member(
-                new Name("죠르디"),
-                new Nickname("jordy"),
+                new Name(NAME),
+                new Nickname(NICKNAME),
                 MemberStatus.AUTHORIZING,
                 Rank.DIAMOND,
-                "05552",
-                new Address("서울시 영등포구 영등포동 포동길 12", "101호"),
-                new Tel("01015251525"),
+                ZIP_CODE,
+                new Address(ADDRESS, ADDRESS_DETAIL),
+                new Tel(TEL),
                 Gender.M,
-                new Age(15),
-                "password"
+                new Age(AGE),
+                PASSWORD
         );
         // when
         // then
