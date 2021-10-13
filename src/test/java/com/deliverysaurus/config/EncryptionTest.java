b081a607b4442c1588504df160fa75d8bfa40881
bbbnam <bbbnam@naver.com>
2021-10-13 17:15:06 +0900

    test : 암호화 모듈 테스트

--- src/test/java/com/deliverysaurus/config/EncryptionTest.java
+++ src/test/java/com/deliverysaurus/config/EncryptionTest.java
@@ -1,53 +1,60 @@
-package com.deliverysaurus.common.encryption;
+package com.deliverysaurus.config;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@SpringBootTest
 class EncryptionTest {
+
+    @Autowired
+    private Encryption encryption;
 
     @DisplayName("aes256 암호화 테스트")
     @Test
     void getEncrypted_aes256() throws Exception {
-        Encryption encryption = new Encryption(EncryptionStrategy.AES256);
 
-        String encrypted = encryption.getEncrypted("1234");
+        EncryptionStrategy aes256 = encryption.get("AES256");
 
-        assertThat(encrypted).isEqualTo(encryption.getEncrypted("1234"));
+        String encrypted = aes256.encrypt("1234");
+
+        assertThat(encrypted).isEqualTo(aes256.encrypt("1234"));
     }
 
     @DisplayName("aes256 복호화 테스트")
     @Test
     void getDecrypted_aes256() throws Exception {
         String targetText = "12345";
-        Encryption encryption = new Encryption(EncryptionStrategy.AES256);
+        EncryptionStrategy aes256 = encryption.get("AES256");
 
-        String encrypted = encryption.getEncrypted(targetText);
+        String encrypted = aes256.encrypt(targetText);
 
-        assertThat(encryption.getDecrypted(encrypted)).isEqualTo(targetText);
+        assertThat(aes256.decrypt(encrypted)).isEqualTo(targetText);
     }
 
     @DisplayName("aes256 암복호화 텍스트 비교")
     @Test
     void isMatch_aes256() throws Exception {
         String targetText = "12345";
-        Encryption encryption = new Encryption(EncryptionStrategy.AES256);
+        EncryptionStrategy aes256 = encryption.get("AES256");
 
-        String encrypted = encryption.getEncrypted(targetText);
+        String encrypted = aes256.encrypt(targetText);
 
-        assertThat(encryption.isMatched(targetText, encrypted)).isTrue();
-        assertThat(encryption.isMatched(targetText + "6", encrypted)).isFalse();
+        assertThat(aes256.isMatch(targetText, encrypted)).isTrue();
+        assertThat(aes256.isMatch(targetText + "6", encrypted)).isFalse();
     }
 
     @DisplayName("bcrypt 암호화 테스트")
     @Test
     void getEncrypted_bcrypt() throws Exception {
         String targetText = "1234";
-        Encryption encryption = new Encryption(EncryptionStrategy.BYCRYPT);
+        EncryptionStrategy bcrypt = encryption.get("Bcrypt");
 
-        String encrypted = encryption.getEncrypted(targetText);
+        String encrypted = bcrypt.encrypt(targetText);
 
         assertThat(encrypted).isNotEqualTo(targetText);
         assertThat(encrypted.length()).isEqualTo(60); //해시화된 길이값 항상 같음
@@ -57,9 +64,9 @@ class EncryptionTest {
     @Test
     void getDecrypted_bcrypt() throws Exception {
         String targetText = "12345";
-        Encryption encryption = new Encryption(EncryptionStrategy.BYCRYPT);
+        EncryptionStrategy bcrypt = encryption.get("Bcrypt");
 
-        String encrypted = encryption.getEncrypted(targetText);
+        String encrypted = bcrypt.encrypt(targetText);
 
         Throwable exception = assertThrows(RuntimeException.class, () -> {
             throw new RuntimeException("Bcrypt는 복호화가 불가합니다.");
@@ -71,11 +78,11 @@ class EncryptionTest {
     @Test
     void isMatch_bcrypt() throws Exception {
         String targetText = "12345";
-        Encryption encryption = new Encryption(EncryptionStrategy.BYCRYPT);
+        EncryptionStrategy bcrypt = encryption.get("Bcrypt");
 
-        String encrypted = encryption.getEncrypted(targetText);
+        String encrypted = bcrypt.encrypt(targetText);
 
-        assertThat(encryption.isMatched(targetText, encrypted)).isTrue();
-        assertThat(encryption.isMatched(targetText + "6", encrypted)).isFalse();
+        assertThat(bcrypt.isMatch(targetText, encrypted)).isTrue();
+        assertThat(bcrypt.isMatch(targetText + "6", encrypted)).isFalse();
     }
 }
